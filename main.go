1560a13fa82583ed65606711ea395994ac95d591
veapach <egor_chapaev@bk.ru>
2025-01-25 12:53:17 +0300

    created readme

--- main.go
+++ main.go
@@ -1,56 +1,29 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type album struct {
-	ID     string  `json: "id"`
-	Title  string  `json: "title"`
-	Artist string  `json: "artsist"`
-	Price  float64 `json: "price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+type Test struct {
+	Message string `json:"message"`
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
-
-	router.Run("localhost:8080")
+	http.HandleFunc("/", homePage)
+	http.ListenAndServe(":8080", nil)
 }
 
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Connected to the home page\n")
+	decoder := json.NewDecoder(r.Body)
+	var t Test
+	err := decoder.Decode(&t)
+	if err != nil {
+		panic(err)
 	}
-
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusFound, gin.H{"message": "Album not found"})
+	fmt.Fprintf(w, "Message: %s", t.Message)
 }
